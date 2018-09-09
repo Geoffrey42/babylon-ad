46d9a1dc19138c201bbba18dba6600610144e8d2
Geoffrey42 <geoffreygane@gmail.com>
2018-09-09 10:06:24 +0200

    chors: remove useless comments and import

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,12 +1,6 @@
 const mongoose = require("mongoose");
-const Event = require("../models/event");
 
 const MLAB_URI = process.env.MLAB_URI;
-console.log("MLAB_URI: ", MLAB_URI);
-// const options = {
-//   reconnectTries: Number.MAX_VALUE,
-//   poolSize: 10
-// };
 
 mongoose
   .connect(
