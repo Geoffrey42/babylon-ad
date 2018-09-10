5494f8394091b4f24a9494e5e11e430f3b0c9b89
Geoffrey42 <geoffreygane@gmail.com>
2018-09-11 01:15:50 +0200

    fix: remove route dealing with geolocalization because not ready yet

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ EventController = require("../controllers/event-controller");
 module.exports = server => {
   server.get("/api/events", EventController.readAll);
   server.get("/api/events/:id", EventController.readById);
-  server.post("/api/events/geo", EventController.readByCoordinates);
 
   server.post("/api/event", EventController.create);
   server.delete("/api/event/:id", EventController.delete);
